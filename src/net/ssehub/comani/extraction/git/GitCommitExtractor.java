305391df55d50918e098c81eafb627759c5aee6b
Christian Kröher <kroeher@sse.uni-hildesheim.de>
2019-02-01 13:35:45 +0100

    Revert fix Linux/OpenJDK-specific commit id bug
    See commit cd0ed8228ca2d8fd8c5636813adbffd2dd0df536
    
    The actual bug is the use of unnecessary quotation marks as part of the
    git command, which are not escaped as expected and, hence, lead to
    additional quotation marks in the results.
    
    Changes from the commit above are reverted and the marks are deleted
    from the command

--- src/net/ssehub/comani/extraction/git/GitCommitExtractor.java
+++ src/net/ssehub/comani/extraction/git/GitCommitExtractor.java
@@ -53,7 +53,7 @@ public class GitCommitExtractor extends AbstractCommitExtractor {
      * <br>
      * Command: <code>git log --pretty=format:"%h"</code>
      */
-    private static final String[] GIT_COMMITS_COMMAND = {"git", "log", "--pretty=format:\"%h\""};
+    private static final String[] GIT_COMMITS_COMMAND = {"git", "log", "--pretty=format:%h"};
     
     /**
      * The constant part of the command for printing the committer date to console. The number of the commit, for which
@@ -391,13 +391,6 @@ public class GitCommitExtractor extends AbstractCommitExtractor {
             // We assume that the standard output stream of the process executed above contains the commit numbers
             String commitLog = executionResult.getStandardOutputData();
             if (commitLog != null && !commitLog.isEmpty()) {
-                /*
-                 * On Linux/OpenJDK-10.0.2 the individual commit numbers are surrounded by additional quotation marks
-                 * while reading the InputStream of the executed command above. These additional marks result in an
-                 * error, if they are used in further commands, like for retrieving the committer date. Hence, we need
-                 * to ensure here that these quotation marks are not present in the result of this method.    
-                 */
-                commitLog = commitLog.replaceAll("\"", "");
                 commitNumbers = commitLog.split("\n");
             } else {
                 logger.log(ID, "Commit log is empty", "No commit numbers to extract", MessageType.ERROR);
